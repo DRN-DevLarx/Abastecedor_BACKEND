7e6455836f82180325665924a4182f1d233a325a
DRN-DevLarx <drndevlarx@gmail.com>
2025-12-28 14:11:33 -0600

    config backend

--- abastecedor/asgi.py
+++ abastecedor/asgi.py
@@ -1,16 +1,6 @@
-"""
-ASGI config for plantilla project.
-
-It exposes the ASGI callable as a module-level variable named ``application``.
-
-For more information on this file, see
-https://docs.djangoproject.com/en/5.1/howto/deployment/asgi/
-"""
-
 import os
+from django.core.wsgi import get_wsgi_application
 
-from django.core.asgi import get_asgi_application
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'abastecedor.settings')
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'plantilla.settings')
-
-application = get_asgi_application()
+application = get_wsgi_application()
