5490d6ad7a761a323b704c3e76fb0c72e0966190
DRN-DevLarx <drndevlarx@gmail.com>
2026-01-17 04:36:03 -0600

    Nuevos modelos a utilizar

--- api/models.py
+++ api/models.py
@@ -20,11 +20,47 @@ class Categoria(models.Model):
 class Proveedor(models.Model):
     nombre = models.CharField(max_length=200)
     telefono = models.CharField(max_length=20, blank=True, null=True)
+    telefono2 = models.CharField(max_length=20, blank=True, null=True)
     correo = models.EmailField(blank=True, null=True)
     direccion = models.TextField(blank=True, null=True)
 
     def __str__(self):
         return self.nombre
+
+# -------------------------------
+# Imagenes del carrusel Inicial
+# -------------------------------
+class ImagenesCarrusel(models.Model):
+    subida_por = models.ForeignKey(User, on_delete=models.CASCADE)
+    estado = models.CharField(max_length=15, default="activa")
+    url = models.TextField()
+    fecha_subida = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"{self.modificado_Por} - {self.estado}"
+
+# -------------------------------
+# Contenido estatico de la pagina
+# -------------------------------
+class ContenidoEstatico(models.Model):
+    titulo = models.CharField(max_length=50)
+    contenido = models.TextField()
+    modificado_por = models.ForeignKey(User, on_delete=models.CASCADE)
+    actualizado_el = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return f"{self.titulo} - {self.modificado_Por} - {self.actualizado_el}"
+
+# -------------------------------
+# Contenido estatico de la pagina
+# -------------------------------
+class Comentarios(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    comentario = models.TextField()
+    publicado_el = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"{self.user} - {self.comentario} - {self.publicado_el}"
 
 # -------------------------------
 # Consultas
@@ -39,6 +75,18 @@ class Consultas(models.Model):
 
     def __str__(self):
         return f"{self.nombre_completo} - {self.mensaje}"
+
+# -------------------------------
+# Comentarios sobre nosotros
+# -------------------------------
+# class Comentarios(models.Model):
+#     user = models.CharField(max_length=10, blank=True, null=True)
+#     mensaje = models.CharField(max_length=100)
+#     fecha = models.DateTimeField(auto_now_add=True)
+
+#     def __str__(self):
+#         return f"{self.user} - {self.mensaje}"
+
 
 # -------------------------------
 # Producto
@@ -93,57 +141,82 @@ class ImagenesUsuario(models.Model):
     def __str__(self):
         return self.user
 
+
+class Carrito(models.Model):
+
+    ESTADOS = (
+        ("ACTIVO", "Activo"),
+        ("PAGO", "En proceso de pago"),
+        ("COMPLETADO", "Completado"),
+        ("CANCELADO", "Cancelado"),
+    )
+
+    usuario = models.ForeignKey(User, on_delete=models.CASCADE, related_name="carritos")
+    estado = models.CharField(max_length=15, choices=ESTADOS, default="ACTIVO")
+    subtotal = models.DecimalField(max_digits=10, decimal_places=2, default=0)
+    total = models.DecimalField(max_digits=10, decimal_places=2, default=0)
+    fecha_creacion = models.DateTimeField(auto_now_add=True)
+    fecha_actualizacion = models.DateTimeField(auto_now=True)
+
+    def recalcular_totales(self):
+        subtotal = sum(item.subtotal for item in self.items.all())
+        self.subtotal = subtotal
+        self.total = subtotal
+        self.save(update_fields=["subtotal", "total"])
+
+    def __str__(self):
+        return f"Carrito #{self.id} - {self.usuario.username} - {self.estado}"
+
+
+class DetalleCarrito(models.Model):
+    carrito = models.ForeignKey(Carrito, on_delete=models.CASCADE, related_name="items")
+    producto = models.ForeignKey(Producto, on_delete=models.CASCADE)
+    cantidad = models.PositiveIntegerField()
+    precio_unitario = models.DecimalField(max_digits=10, decimal_places=2)
+    subtotal = models.DecimalField(max_digits=10, decimal_places=2)
+
+    class Meta:
+        unique_together = ("carrito", "producto")
+
+    def save(self, *args, **kwargs):
+        self.subtotal = self.precio_unitario * self.cantidad
+        super().save(*args, **kwargs)
+
+    def __str__(self):
+        return f"{self.producto.nombre} x {self.cantidad} x {self.subtotal}"
+
+
 # -------------------------------
 # Pedido
 # -------------------------------
+# 
 class Pedido(models.Model):
     ESTADOS = [
         ("pendiente", "Pendiente"),
-        ("en_proceso", "En proceso"),
+        ("en_preparacion", "En preparación"),
+        ("empacado", "Empacado"),
+        ("enviado", "Enviado"),
         ("entregado", "Entregado"),
         ("cancelado", "Cancelado"),
     ]
 
     cliente = models.ForeignKey(User, on_delete=models.CASCADE)
     fecha = models.DateTimeField(auto_now_add=True)
+
     estado = models.CharField(max_length=20, choices=ESTADOS, default="pendiente")
+    pagado = models.BooleanField(default=False)
+
+    fecha_pago = models.DateTimeField(null=True, blank=True)
+    metodo_pago = models.CharField(max_length=50, null=True, blank=True)
 
     def total(self):
-        return sum(detalle.subtotal() for detalle in self.detallepedido_set.all())
+        return sum(d.subtotal() for d in self.detallepedido_set.all())
 
     def __str__(self):
-        return f"Pedido {self.id} - {self.cliente.user.username}"
-
+        return f"Pedido {self.id} - {self.cliente.username}"
 
 class DetallePedido(models.Model):
     pedido = models.ForeignKey(Pedido, on_delete=models.CASCADE)
-    producto = models.ForeignKey(Producto, on_delete=models.CASCADE)
-    cantidad = models.PositiveIntegerField()
-    precio_unitario = models.DecimalField(max_digits=8, decimal_places=2)
-
-    def subtotal(self):
-        return self.cantidad * self.precio_unitario
-
-    def __str__(self):
-        return f"{self.cantidad} x {self.producto.nombre}"
-
-# -------------------------------
-# Venta
-# -------------------------------
-class Venta(models.Model):
-    pedido = models.OneToOneField(Pedido, on_delete=models.SET_NULL, null=True, blank=True)  # opcional
-    cliente = models.ForeignKey(InformacionUsuario, on_delete=models.SET_NULL, null=True, blank=True)
-    fecha = models.DateTimeField(auto_now_add=True)
-    empleado = models.ForeignKey(User, on_delete=models.SET_NULL, null=True, blank=True, related_name="ventas_registradas")
-
-    def total(self):
-        return sum(detalle.subtotal() for detalle in self.detalleventa_set.all())
-
-    def __str__(self):
-        return f"Venta {self.id} - {self.fecha}"
-
-class DetalleVenta(models.Model):
-    venta = models.ForeignKey(Venta, on_delete=models.CASCADE)
     producto = models.ForeignKey(Producto, on_delete=models.CASCADE)
     cantidad = models.PositiveIntegerField()
     precio_unitario = models.DecimalField(max_digits=8, decimal_places=2)
